e6f78deae753d7c3fef0ea755eef972ccc57d2bf
mrlalex <mierlutalex01@gmail.com>
2025-01-22 04:03:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express");
+ const express = require("express");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servește fișierele statice din folderul 'dist' (sau alt folder unde e index.html)
-app.use(express.static(path.join(__dirname, "dist")));
+// Servește fișierele statice din rădăcina proiectului
+app.use(express.static(path.join(__dirname)));
 
-// Dacă nicio altă rută nu este potrivită, servește 'index.html'
+// Dacă nicio altă rută nu este potrivită, servește 'index.html' din rădăcină
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
